688bea1f2a2d832de01856b7777d98a0cc2a8025
Chris Whitten <christopher.whitten@microsoft.com>
2019-02-15 06:29:46 -0800

    Some basic form adjustments

--- src/App.js
+++ src/App.js
@@ -2,31 +2,73 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "react-jsonschema-form";
 
-import schema from "./appschema.json";
+import schema3 from "./appschema3.json";
 
-const onChange = ({ formData }, e) => {
-  console.log("onChange", formData, e);
-};
 class App extends Component {
+  static defaultProps = {
+    onChange: () => {},
+    formData: {}
+  };
+
+  isRootId = id => {
+    return id.indexOf("root") !== -1;
+  };
+
+  isUnecessaryLabel = label => {
+    return (
+      label.indexOf("Microsoft.") === -1 &&
+      label.indexOf("Dialog") === -1 &&
+      label.indexOf("sequence") === -1
+    );
+  };
+
+  fieldTemplate = props => {
+    const { id, classNames, label, required, children } = props;
+
+    return (
+      <React.Fragment>
+        {this.isRootId(id) && (
+          <div className={classNames}>
+            {children.map(c => {
+              if (
+                c &&
+                c.props &&
+                (c.props.name === "$type" ||
+                  c.props.name === "$ref" ||
+                  c.props.name === "$id")
+              ) {
+                return null;
+              }
+              return c;
+            })}
+          </div>
+        )}
+        {!this.isRootId(id) && (
+          <div className={classNames}>
+            {this.isUnecessaryLabel(label) && (
+              <label htmlFor={id}>
+                {label}
+                {required ? "*" : null}
+              </label>
+            )}
+            {children}
+          </div>
+        )}
+      </React.Fragment>
+    );
+  };
+
   render() {
     return (
-      <div className="App" style={{ width: "700px", marginLeft: "200px" }}>
+      <div className="App" style={{ margin: "15px 15px 15px 15px" }}>
         <Form
+          FieldTemplate={this.fieldTemplate}
           noValidate
           className="schemaForm"
-          onChange={onChange}
-          formData={{}}
-          // onSubmit={onSubmit}
-          schema={schema}
-          uiSchema={{}}
-        >
-          {/* <button
-            // ref={btn => {
-            //   submitButton = btn;
-            // }}
-            className="hidden"
-          /> */}
-        </Form>
+          onChange={this.props.onChange}
+          formData={this.props.formData}
+          schema={schema3}
+        />
       </div>
     );
   }
